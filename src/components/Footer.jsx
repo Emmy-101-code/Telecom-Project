2f9d54b6a6fde80208aa698db28a4fb79b07d648
EMMANUEL OKUNOLA <eokunola99@gmail.com>
2025-11-11 21:46:10 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 export default function Footer(){
   return (
     <footer className="mt-12 bg-white border-t">
-      <div className="max-w-7xl mx-auto px-6 py-8 text-sm text-slate-600">© 2025 EmmyPay Telecom. All rights reserved.</div>
+     {/* <div className="max-w-7xl mx-auto px-6 py-8 text-sm text-slate-600">© 2025 EmmyPay Telecom. All rights reserved.</div> */}
+   <div className="max-w-7xl mx-auto px-6 py-8 text-sm text-slate-600 text-center">
+  © 2025 EmmyPay Telecom. All rights reserved.
+</div>
     </footer>
   )
 }
